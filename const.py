f9a2e2d8aa72d335144e5f4ee9499b2b0a47e61f
ho <choi.scholarg@gmail.com>
2024-12-06 16:52:21 +0000

    action space defined

---
+++ const.py
@@ -0,0 +1,13 @@
+snp_indices_assets = {
+    "SPLRCT": "S&P 500 Information Technology",  # done
+    "SPLRCL": "S&P 500 Telecom Services",  # done
+    "SPLRCM": "S&P 500 Materials",  # done
+    "SPLRCREC": "S&P 500 Real Estate",  # 20020101부터 사용가능  # done
+    "SPLRCS": "S&P 500 Consumer Staples",  # done
+    "SPSY": "S&P 500 Financials",  # done
+    "SPNY": "S&P 500 Energy",  # done
+    "SPXHC": "S&P 500 Health Care",  # done, # 중간에 정보 손실 interpolation 해버리기
+    "SPLRCD": "S&P 500 Consumer Discretionary",  # done
+    "SPLRCI": "S&P 500 Industrials",  # done
+    "SPLRCU": "S&P 500 Utilities",  # done
+}
